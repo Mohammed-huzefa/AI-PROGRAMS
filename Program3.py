52e5edaec019dcba851591b92cc96867de7ea7b5
Mohammed Huzefa <mdhuzefa1795@gmail.com>
2026-03-05 12:07:44 +0530

    Update Program3.py

--- Program3.py
+++ Program3.py
@@ -1,91 +1,28 @@
 import heapq
 
-class Node:
-    def __init__(self, state, parent=None, action=None, cost=0, heuristic=0):
-        self.state = state
-        self.parent = parent
-        self.action = action
-        self.cost = cost
-        self.heuristic = heuristic
+def h(n,g):
+    return abs(ord(n)-ord(g))
 
-    def __lt__(self, other):
-        return (self.cost + self.heuristic) < (other.cost + other.heuristic)
+def astar(start,goal,graph):
+    q=[(h(start,goal),0,start,[start])]
+    v=set()
 
-def parse_graph_input():
-    graph = {}
-    num_edges = int(input("Enter the number of edges: "))
+    while q:
+        f,c,n,path=heapq.heappop(q)
 
-    for _ in range(num_edges):
-        u, v, cost = input("Enter an edge (format: u v cost): ").split()
-        cost = int(cost)
-
-        if u not in graph:
-            graph[u] = []
-        if v not in graph:
-            graph[v] = []
-
-        graph[u].append((v, cost))
-        graph[v].append((u, cost))
-
-    return graph
-
-def astar_search(start_state, goal_test, successors, heuristic):
-    frontier = []
-    heapq.heappush(frontier, Node(start_state, None, None, 0, heuristic(start_state)))
-
-    explored = set()
-
-    while frontier:
-        current_node = heapq.heappop(frontier)
-        current_state = current_node.state
-
-        if goal_test(current_state):
-            path = []
-            while current_node.parent is not None:
-                path.append((current_node.action, current_node.state))
-                current_node = current_node.parent
-            path.reverse()
+        if n==goal:
             return path
 
-        explored.add(current_state)
+        v.add(n)
 
-        for action, successor_state, step_cost in successors(current_state):
-            if successor_state not in explored:
-                new_cost = current_node.cost + step_cost
-                new_node = Node(successor_state, current_node, action, new_cost, heuristic(successor_state))
-                heapq.heappush(frontier, new_node)
+        for nb,w in graph.get(n,[]):
+            if nb not in v:
+                heapq.heappush(q,(c+w+h(nb,goal),c+w,nb,path+[nb]))
 
-    return None
+g={'A':[('B',1),('C',3)],
+   'B':[('D',2)],
+   'C':[('D',4)]}
 
-if __name__ == "__main__":
+p=astar('A','D',g)
 
-    print("Define the graph:")
-    graph = parse_graph_input()
-
-    start_state = input("Enter the start state: ")
-    goal_state = input("Enter the goal state: ")
-
-    def goal_test(state):
-        return state == goal_state
-
-    def successors(state):
-        successors_list = []
-        for neighbor, cost in graph.get(state, []):
-            action = f"Move to {neighbor}"
-            successor_state = neighbor
-            step_cost = cost
-            successors_list.append((action, successor_state, step_cost))
-        return successors_list
-
-    def heuristic(state):
-        heuristic_values = {key: abs(ord(key) - ord(goal_state)) for key in graph.keys()}
-        return heuristic_values.get(state, float('inf'))
-
-    path = astar_search(start_state, goal_test, successors, heuristic)
-
-    if path:
-        print("Path found:")
-        for action, state in path:
-            print(f"Action: {action}, State: {state}")
-    else:
-        print("No path found.")
+print("Path:",p)
