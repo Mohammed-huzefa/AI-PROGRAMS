987d0dce7fcf41107d3643a0bcde3ab69c52fad5
Mohammed Huzefa <mdhuzefa1795@gmail.com>
2026-03-05 12:03:42 +0530

    Update Program5.py

--- Program5.py
+++ Program5.py
@@ -1,56 +1,23 @@
-def is_safe(board, row, col):
-    for i in range(row):
-        if board[i][col] == 1:
+def safe(b,r,c):
+    for i in range(r):
+        if b[i]==c or abs(b[i]-c)==abs(i-r):
             return False
-    
-    i, j = row, col
-    while i >= 0 and j >= 0:
-        if board[i][j] == 1:
-            return False
-        i -= 1
-        j -= 1
-    
-    i, j = row, col
-    while i >= 0 and j < len(board):
-        if board[i][j] == 1:
-            return False
-        i -= 1
-        j += 1
-    
     return True
 
-def solve_queens(board, row):
-    n = len(board)
-    
-    if row >= n:
+def solve(b,r,n):
+    if r==n:
+        for i in range(n):
+            for j in range(n):
+                print("Q" if b[i]==j else ".",end=" ")
+            print()
         return True
-    
-    for col in range(n):
-        if is_safe(board, row, col):
-            board[row][col] = 1
-            
-            if solve_queens(board, row + 1):
+    for c in range(n):
+        if safe(b,r,c):
+            b[r]=c
+            if solve(b,r+1,n):
                 return True
-            
-            board[row][col] = 0
-    
     return False
 
-def print_board(board):
-    n = len(board)
-    for i in range(n):
-        for j in range(n):
-            print(board[i][j], end=" ")
-        print()
-
-def solve_8queens():
-    n = 8
-    board = [[0] * n for _ in range(n)]
-    
-    if solve_queens(board, 0):
-        print("Solution found:")
-        print_board(board)
-    else:
-        print("No solution exists.")
-
-solve_8queens()
+n=8
+board=[-1]*n
+solve(board,0,n)
