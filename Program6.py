cba2fcbc29bf46d6d1a4f397df759063d4ca5fc7
Mohammed Huzefa <mdhuzefa1795@gmail.com>
2026-04-23 00:38:00 +0530

    Update Program6.py

--- Program6.py
+++ Program6.py
@@ -1,34 +1,37 @@
-def tsp(d):
-    n=len(d)
-    tour=[0]
-    visited={0}
-    cost=0
+import sys
 
-    while len(visited)<n:
-        m=10**9
-        for j in range(n):
-            if j not in visited and d[tour[-1]][j]<m:
-                m=d[tour[-1]][j]
-                x=j
-        tour.append(x)
-        visited.add(x)
-        cost+=m
-
-    cost+=d[tour[-1]][0]
+def nearest_neighbor_tsp(distances):
+    num_cities = len(distances)
+    
+    tour = [0]
+    visited = set([0])
+    
+    current_city = 0
+    total_distance = 0
+    
+    while len(visited) < num_cities:
+        nearest_city = None
+        min_distance = sys.maxsize
+        
+        for next_city in range(num_cities):
+            if next_city not in visited and distances[current_city][next_city] < min_distance:
+                nearest_city = next_city
+                min_distance = distances[current_city][next_city]
+        
+        tour.append(nearest_city)
+        visited.add(nearest_city)
+        total_distance += min_distance
+        current_city = nearest_city
+    
     tour.append(0)
+    total_distance += distances[current_city][0]
+    
+    return tour, total_distance
 
-    return tour,cost
-
-
-n=int(input("Enter number of cities: "))
-d=[]
-
-print("Enter distance matrix:")
-for i in range(n):
-    row=list(map(int,input().split()))
-    d.append(row)
-
-tour,cost=tsp(d)
-
-print("TSP Tour:",tour)
-print("Total Cost:",cost)
+if __name__ == "__main__":
+    distances = [[0, 4, 8, 9, 12], [4, 0, 6, 8, 9], [8, 6, 0, 10, 11], [9, 8, 10, 0, 7], [12, 9, 11, 7, 0]]
+    
+    tour, total_distance = nearest_neighbor_tsp(distances)
+    
+    print("Nearest Neighbor TSP Tour:", tour)
+    print("Total Distance:", total_distance)
