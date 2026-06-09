d1d753d8f1eb82e0ccdb77a66ef11e2b2eee5d95
Mohammed Huzefa <mdhuzefa1795@gmail.com>
2026-06-10 03:01:28 +0530

    Refactor tic-tac-toe game functions and logic

--- Program9.py
+++ Program9.py
@@ -1,53 +1,41 @@
-def print_board(board):
-    for row in board:
-        print(" | ".join(row))
-        print("-" * 9)
+def show(b):
+    for r in b:
+        print(" | ".join(r))
+        print("-"*9)
 
-def check_winner(board, player):
-    for row in board:
-        if all(cell == player for cell in row):
-            return True
-    for col in range(3):
-        if all(board[row][col] == player for row in range(3)):
-            return True
-    if all(board[i][i] == player for i in range(3)):
+def win(b,p):
+    if any(all(c==p for c in r) for r in b):
         return True
-    if all(board[i][2-i] == player for i in range(3)):
+    if any(all(b[r][c]==p for r in range(3)) for c in range(3)):
+        return True
+    if all(b[i][i]==p for i in range(3)):
+        return True
+    if all(b[i][2-i]==p for i in range(3)):
         return True
     return False
 
-def is_full(board):
-    return all(cell != ' ' for row in board for cell in row)
+b=[[' ']*3 for _ in range(3)]
+p='X'
 
-def tic_tac_toe():
-    board = [[' ' for _ in range(3)] for _ in range(3)]
-    current_player = 'X'
+while 1:
+    show(b)
+    r=int(input("Row: "))-1
+    c=int(input("Col: "))-1
 
-    while True:
-        print_board(board)
-        print(f"Player {current_player}'s turn.")
-        row = int(input("Enter row (1-3): "))
-        col = int(input("Enter column (1-3): "))
-        row -= 1
-        col -= 1
-        
-        if board[row][col] == ' ':
-            board[row][col] = current_player
-        else:
-            print("Invalid move! Try again.")
-            continue
+    if b[r][c]!=' ':
+        print("Invalid")
+        continue
 
-        if check_winner(board, current_player):
-            print_board(board)
-            print(f"Player {current_player} wins!")
-            break
+    b[r][c]=p
 
-        if is_full(board):
-            print_board(board)
-            print("It's a tie!")
-            break
+    if win(b,p):
+        show(b)
+        print("Player",p,"wins!")
+        break
 
-        current_player = 'O' if current_player == 'X' else 'X'
+    if all(x!=' ' for row in b for x in row):
+        show(b)
+        print("Tie!")
+        break
 
-if __name__ == "__main__":
-    tic_tac_toe()
+    p='O' if p=='X' else 'X'
